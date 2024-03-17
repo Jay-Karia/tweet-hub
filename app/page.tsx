adc7e4359f1612db9ca5945b7f2b8ba0a1a7faf7
Jay-Karia <jay.sanjay.karia@gmail.com>
2024-03-17 22:06:42 +0530

    setup shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex space-x-1">
-      <Image
-        src="/logo.svg"
-        alt="TweetHub Logo"
-        width={30}
-        height={30}
-      />
-      <h1 className="text-blue-400 font-medium">
-        Hello TweetHub
-      </h1>
+    <div className="flex flex-col space-y-5 w-max ml-2">
+      <div className="flex flex-row space-x-1">
+        <Image
+          src="/logo.svg"
+          alt="TweetHub Logo"
+          width={30}
+          height={30}
+        />
+        <h1 className="text-blue-400 font-medium">
+          Hello TweetHub
+        </h1>
+      </div>
+      <Button variant="custom">
+        Tweet
+      </Button>
     </div>
   );
 }
